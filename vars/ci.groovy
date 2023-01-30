2818f49358c1fa5f73707b98b258f617195086a9
sai charan <saicharanreddy14396@gmail.com>
2023-01-30 10:22:17 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,41 +1,51 @@
 def call() {
-    pipeline {
+    try {
+        pipeline {
 
-        agent {
-            label 'workstation'
-        }
+            agent {
+                label 'workstation'
+            }
 
-        stages {
+            stages {
 
-            stage('Compile/Build') {
-                steps {
-                    script {
-                        common.compile()
+                stage('Compile/Build') {
+                    steps {
+                        script {
+                            common.compile()
+                        }
                     }
                 }
-            }
 
-            stage('Unit Tests') {
-                steps {
-                    echo 'Unit Tests'
+                stage('Unit Tests') {
+                    steps {
+                        script {
+                            common.unittests()
+                        }
+                    }
                 }
-            }
 
-            stage('Quality Control') {
-                steps {
-                    echo 'Quality Control'
+                stage('Quality Control') {
+                    environment {
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
+                    steps {
+
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.11.33:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                    }
                 }
-            }
 
-            stage('Upload Code to Centralized Place') {
-                steps {
-                    echo 'Upload'
+                stage('Upload Code to Centralized Place') {
+                    steps {
+                        echo 'Upload'
+                    }
                 }
-            }
 
+
+            }
 
         }
-
+    } catch(Exception e) {
+        common.email("Failed")
     }
 }
-
